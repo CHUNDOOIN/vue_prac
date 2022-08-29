4ea526b51141302fc1f227f3e2288e59f16d2713
CHUNDOOIN <chun.doo.in@gmail.com>
2022-08-29 16:21:48 +0900

    22.08.29 vue강의

--- src/Modal.vue
+++ src/Modal.vue
@@ -6,11 +6,15 @@
             <p>{{ room[clickMove].content }}</p>
             <!-- <input @input="month = $event.target.value" /> -->
             <input v-model="month" />
-            <input type="range" min="1" max="12" v-model="month" />
+            <input type="range" min="3" max="12" v-model="month" />
 
             <p>
                 {{ month }}개월 선택함 :
-                {{ Number(room[clickMove].price * Number(month)) }}원
+                {{
+                    Number(
+                        room[clickMove].price * Number(month)
+                    ).toLocaleString()
+                }}원
             </p>
             <button @click="closeSend">닫기</button>
         </div>
@@ -22,7 +26,7 @@ export default {
     name: "Modal",
     data() {
         return {
-            month: 1,
+            month: 3,
         };
     },
 
@@ -30,12 +34,20 @@ export default {
         month(a) {
             if (isNaN(a) === true) {
                 alert("숫자가 아닌데요?");
-                this.month = 1;
+                this.month = 3;
+                console.log("3으로 강제적으로 변경됩니다.");
             }
 
             if (a >= 13) {
                 alert("최대 12개월!!!");
-                this.month = 1;
+                this.month = 3;
+                console.log("3으로 강제적으로 변경됩니다.");
+            }
+
+            if (a === " ") {
+                alert("스페이스는 안됩니다. (단호)");
+                this.month = 3;
+                console.log("3으로 강제적으로 변경됩니다.");
             }
         },
     },
@@ -50,7 +62,7 @@ export default {
         },
     },
     updated() {
-        if (this.month === "2") {
+        if (this.month === "2" || this.month === "1" || this.month === "0") {
             alert("우린 3개월부터유");
             this.month = 3;
             console.log("3으로 강제적으로 변경됩니다.");
